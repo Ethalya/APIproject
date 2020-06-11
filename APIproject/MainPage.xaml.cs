ae7a50e745f5e418a6f8dfd6b93950df2f392e06
Ethalya <siyao@interia.pl>
2020-06-11 20:42:22 +0200

    x

--- APIproject/MainPage.xaml.cs
+++ APIproject/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace APIproject
@@ -16,6 +17,28 @@ namespace APIproject
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+        }
+
+        public ICommand regPanel
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    App.Current.MainPage = new Register();
+                });
+            }
+        }
+
+        private void btnLogIn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new Panel();
+        }
+
+        private void btnPortfolio_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new Portfolio();
         }
     }
 }
---
+++ APIproject/Portfolio.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace APIproject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Portfolio : ContentPage
+    {
+        public Portfolio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MainPage();
+        }
+    }
+}
